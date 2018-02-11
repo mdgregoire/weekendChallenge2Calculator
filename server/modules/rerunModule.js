2e56de38401db0362b2e0029108c08850e7f5286
Michael Gregoire <mdgregoire@gmail.com>
2018-02-11 12:41:05 -0600

    typo

--- server/modules/rerunModule.js
+++ server/modules/rerunModule.js
@@ -1,11 +1,11 @@
 const historyArray = require('./storageModule');
 
 function rerun(index){
-  //this is a silly line if code, the commented out section below has the same functionality but reads eaiser
+  //this is a silly line of code, the commented out section below has the same functionality but reads eaiser
   historyArray.history.push(historyArray.history[index.index]);
   // let theArray = historyArray.history;
   // let theIndex = index.index;
-  // theArray.push(theArray[theIndex])
+  // theArray.push(theArray[theIndex]);
   return historyArray.history;
 }//end rerun function
 
